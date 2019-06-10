eaddc632a3a429cad3304cd40b2dd8f2ebdcabbd
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-06-09 20:40:39 -0700

    added functions to update and check if levels are locked or unlocked

--- Assets/Scripts/Helper/MainMenuController.cs
+++ Assets/Scripts/Helper/MainMenuController.cs
@@ -7,8 +7,18 @@ using UnityEngine.SceneManagement;
 public class MainMenuController : MonoBehaviour
 {
     public Button[] levels;
-
     public Animator anim;
+    public Sprite[] stageUnlock;
+    public Sprite stageLocked;
+    public Text starScoreText;
+
+
+    private void Start() {
+        GameManager.instance.UpdateLockedLevels();
+        UpdateLevelStatus();
+        UpdateStarScoreText();
+
+    }
 
     public void PlayGame(){
         anim.Play("SlideIn");
@@ -18,7 +28,27 @@ public class MainMenuController : MonoBehaviour
         anim.Play("SlideOut");
     }
 
-    public void LoadLevel(int level){
+    void LoadLevel(int level){
+        GameManager.instance.currentLevel = level;
         SceneManager.LoadScene("Level"+level);
+    }
+
+    public void checkIfLevelIsLocked(int level){
+        if(GameManager.instance.levelsUnlocked[level-1]){
+            LoadLevel(level);
+        }
+    }
+
+    void UpdateLevelStatus(){
+        for(int levelIndex = 1; levelIndex < GameManager.instance.totalNumOfLevels; levelIndex++){
+            if(GameManager.instance.levelsUnlocked[levelIndex]){
+                levels[levelIndex].GetComponent<Image>().sprite = stageUnlock[0];
+                levels[levelIndex].GetComponentInChildren<Text>().text = "Unlocked";
+            }
+        }
+    }
+
+    void UpdateStarScoreText(){
+        starScoreText.text = "Total Stars: " + GameManager.instance.totalStarScore;
     }
 }
